a238aec41887afb87e3543f6f95dfdc420677e1d
IT21358098 <IT21358098@my.sliit.lk>
2023-05-01 20:14:47 +0530

    initial commit yoshitha

---
+++ app/src/main/java/com/example/tourismhelper/tourfragment/TouristHomeFragment.kt
@@ -0,0 +1,19 @@
+package com.example.tourismhelper.tourfragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.tourismhelper.R
+
+class TouristHomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_tourist_home2, container, false)
+    }
+}

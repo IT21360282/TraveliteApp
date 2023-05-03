ff094982d4e30164e019b8b15fc5a0e418374a56
K. A. S. N. Perera <99161169+IT21358234@users.noreply.github.com>
2023-05-03 09:55:50 +0530

    Initial Commit Nilanka 03/05

--- app/src/main/java/com/example/tourismhelper/ResHotelLoginActivity.kt
+++ app/src/main/java/com/example/tourismhelper/ResHotelLoginActivity.kt
@@ -7,6 +7,11 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.values
 
 class ResHotelLoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,13 +22,13 @@ class ResHotelLoginActivity : AppCompatActivity() {
         login?.setTitle("Restaurant/Hotel Login")
 
         var UNorEmail = findViewById<EditText>(R.id.edtLoginUNorEmail)
+        var UNorEmailText = UNorEmail.text.toString()
         var password = findViewById<EditText>(R.id.edtLoginPass)
         var LoginToReg = findViewById<TextView>(R.id.txtViewLoginToReg)
         var btnLogin = findViewById<Button>(R.id.btnSubmitLogin)
 
         btnLogin.setOnClickListener{
-            validate(UNorEmail.text.toString(), password.text.toString())
-            password.text = null
+            Toast.makeText(this, "${UNorEmail.text.toString()}", Toast.LENGTH_SHORT).show()
         }
 
         LoginToReg.setOnClickListener {
@@ -34,11 +39,31 @@ class ResHotelLoginActivity : AppCompatActivity() {
 
     }
 
-    private fun validate(user:String, password:String) {
-        var un = "nilanka"
+    private fun validate(userTxt:String, passwordTxt:String) {
+        val database = FirebaseDatabase.getInstance().getReference("hotel_restaurant_user")
+
+        val userId = "8HPEWixAiKVMSpVI57iV" // Replace with the key of the child node you want to retrieve.
+
+        database.child(userId).child("fullName").addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val name = dataSnapshot.getValue(String::class.java)
+                if (name != null) {
+                    // Display the name in a Toast message.
+                    Toast.makeText(applicationContext, "User name: $name", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // This method will be called if there is an error reading the data from the database.
+            }
+        })
+
+
+
+        /*var un = "nilanka"
         var pass = "SN1234"
         var bType = "restaurant"
-        if(un == user && pass == password){
+        if(un == userTxt && pass == passwordTxt){
             if(bType == "hotel"){
                 Toast.makeText(this, "Login Successfully", Toast.LENGTH_SHORT).show()
                 val intent = Intent(this, ResHotelMainActivity::class.java)
@@ -52,6 +77,7 @@ class ResHotelLoginActivity : AppCompatActivity() {
         }
         else{
             Toast.makeText(this, "Incorrect UserName or Password", Toast.LENGTH_SHORT).show()
-        }
+        }*/
+
     }
 }
